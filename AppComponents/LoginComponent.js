c3c5ed08e17a2ab0810d5463c63fc74ac1ba27d4
kjh08490 <kjh08490@ly.com>
2016-08-22 13:20:24 +0800

    聚合数据news模块的加入

--- AppComponents/LoginComponent.js
+++ AppComponents/LoginComponent.js
@@ -1,6 +1,6 @@
 const React = require('react-native');
 const Colors = require('../commonComponents/Colors');
-const   = require('../config');
+const config = require('../config');
 const CommonStyles = require('../commonComponents/CommonStyles');
 const CommonComponents = require('../commonComponents/CommonComponents');
 const GHService = require('../networkService/GithubServices')
